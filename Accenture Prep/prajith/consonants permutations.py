8e8b8821fe625aaffce41da5bd72919d78895414
Prajith PS <Prajithps8903@gmail.com>
2024-09-26 21:53:08 +0530

    Update consonants permutations.py

--- Accenture Prep/prajith/consonants permutations.py
+++ Accenture Prep/prajith/consonants permutations.py
@@ -1,3 +1,19 @@
+# Vowel Permutation
+
+# You are given a string S and your task is to find and return the count of permutations formed by fixing the positions of the vowels present in the string.
+
+# Note: 
+# Ensure the result is non negative.
+
+# If there are no consonants then return 0.
+# Input Specification:
+
+# input1: A string S
+
+# Output Specification:
+
+# Return an integer value representing the count of permutations formed by fixing the positions of the vowels present in the string.
+
 def fact(n):
     if n == 0 or n == 1:
         return 1
