2d8cce6347b0bd778307ab32c1c0b5ec1eb24505
Prajith PS <Prajithps8903@gmail.com>
2024-09-26 21:31:55 +0530

    Update second_largest_element.py

--- Accenture Prep/prajith/second_largest_element.py
+++ Accenture Prep/prajith/second_largest_element.py
@@ -1,3 +1,19 @@
+# Second Occurrence 
+
+# Charles is given an array A. He wants to find the count of occurences of second largest element in the array. Your task is to help him and return an integer value representing the count of occurrence of the second largest element in an array. 
+
+# note:
+# If the array contains the same elements, then return 0
+# The array has only consecutive elements	
+# Input Specification:
+
+# input 1 : An integer N, representing length of array.
+# Input 2 : An integer array A. 
+
+# Output Specification:
+
+# Return an integer value representing the count of occurrence of the second largest element in an array
+
 def sec(arr,n):
     if n < 2:
         return 0
