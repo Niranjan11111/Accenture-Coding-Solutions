dd20d2ece020ec2a8b3c86322fad60399ca2df7e
Prajith PS <Prajithps8903@gmail.com>
2024-09-26 21:36:59 +0530

    Create hike trail.py

---
+++ Accenture Prep/prajith/hike trail.py
@@ -0,0 +1,15 @@
+# Hike Trial
+
+# You are on a hiking trail represented by an array A of length N, where the trail initially ascends and then descends, forming a single peak. Your task is to find and return an integer value representing the elevation of the summit.
+
+# Input Specification:
+
+# input1: An integer array A 
+# input2: An integer value N denoting the size of A
+
+# Output Specification:
+
+# Return an integer value representing the elevation of the summit.
+
+def hike(arr):
+  return max(arr)
