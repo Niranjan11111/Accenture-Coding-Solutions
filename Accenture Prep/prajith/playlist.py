7c1846ec017ad818fca3ed8521b940a7eb1a8811
Prajith PS <Prajithps8903@gmail.com>
2024-09-26 21:37:58 +0530

    Update playlist.py

--- Accenture Prep/prajith/playlist.py
+++ Accenture Prep/prajith/playlist.py
@@ -1,3 +1,14 @@
+# Alice and Song
+# Alice has a collection of songs represented as a string S where each character represents a song. A playlist is a substring of the given string with exactly K number of songs. She wants to create a playlist that contains maximum number of her favourite song which is 'a'. Your task is to find and return an integer value representing the maximum number of favourite songs that she can get in a single playlist. 
+
+# Input Specification:
+# input1: A string value S representing the collection of songs.
+# input2: An integer value K representing the number of songs in the playlist.
+
+# Output Specification:
+# Return an integer value representing the maximum number of favourite songs that she can get in a single playlist.
+
+
 def playlist(s, k):
     substrings = []
     n = len(s)
