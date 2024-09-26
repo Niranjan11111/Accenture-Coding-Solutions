c2c300fe8dfdb49ccf9934106523338890d37439
Prajith PS <Prajithps8903@gmail.com>
2024-09-26 21:39:11 +0530

    Update multiples of 3 pairs.py

--- Accenture Prep/prajith/multiples of 3 pairs.py
+++ Accenture Prep/prajith/multiples of 3 pairs.py
@@ -1,3 +1,20 @@
+# Product Pair
+
+# You are given an integer array A of length N and your task is to find and return an integer value representing the count of unique pairs whose products are multiples of 3. 
+
+# Note:
+
+# A unique pair means that the elements must be the same regardless of their order. For instance {1, 3} and {3, 1} are considered as the same pair.
+
+# Input Specification:
+
+# input1: An integer value N, representing the size of the array
+# input2: An integer array A
+
+# Output Specification:
+
+# Return an integer value representing the count of unique pairs whose products are multiples of 3.
+
 def count_pairs_multiple_of_3(A):
     seen_pairs = set() 
     for i in range(len(A)):
