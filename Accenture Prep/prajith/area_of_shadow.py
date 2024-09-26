adbdafca7b4932ce8169848170244537a408770e
Prajith PS <Prajithps8903@gmail.com>
2024-09-26 21:30:47 +0530

    Update area_of_shadow.py

--- Accenture Prep/prajith/area_of_shadow.py
+++ Accenture Prep/prajith/area_of_shadow.py
@@ -1,3 +1,19 @@
+# Canopy Area
+# You are developing a feature for an environmental awareness app that helps users to know how much area their tree’s shadow covers. You have the distance D from a tree’s trunk to the edge of the shadow. Your task is to calculate and return an integer value representing the shadow area of the canopy.
+# note: Round off the result to nearest integer
+# Input Specification:
+
+# Input 1 : An integer value D. representing the distance from the tree trunk to the edge of shadow
+
+# Output Specification:
+
+# Return an integer value representing the shadow area of the canopy
+
 def canopy(d):
     return round(3.14*d*d)
-canopy(5)
+n = int(input())
+canopy(n)
+
+
+
+
