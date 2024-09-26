799768229fef49d09f389b618769f817e41efab5
Prajith PS <Prajithps8903@gmail.com>
2024-09-26 21:32:53 +0530

    Update max_product_pair.py

--- Accenture Prep/prajith/max_product_pair.py
+++ Accenture Prep/prajith/max_product_pair.py
@@ -1,3 +1,19 @@
+# Maximum pair product 
+
+# Noah is given an integer array A of length N. He must perform the following operations on the array :
+# Select any integer pairs from the array with their sum equal to 18.
+# From this select the pair with the maximum product such that the first element of the pair is greater than the second element of the pair.
+# Your task is to help Noah find and return a pair in the form of an integer array which satisfies the conditions mentioned	
+# Input Specification:
+
+# input 1 : An integer N, representing length of array.
+# Input 2 : An integer array A. 
+
+# Output Specification:
+
+# Return a pair in the form of an integer array which satisfies the conditions mentioned
+
+
 def mpp(n, arr):
     max_pro = 0
     for i in range(n):
