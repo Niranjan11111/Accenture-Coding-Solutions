22d6f1d8359af157cf0ed651b11b1d6d6eee8e45
Niranjan <raviniranjan428@gmail.com>
2024-09-29 16:49:22 +0530

    noice

--- Accenture Prep/rhymingWords.py
+++ Accenture Prep/rhymingWords.py
@@ -1,26 +1,29 @@
 def rhymingWords(words, target):
-    wordDict = {}
+    max_word = ""
+    max_value = 0
+
     for word in words:
         if word == target:
-            continue 
-        wordDict[word] = 0  
+            continue
 
         rev_word = list(reversed(word))
         rev_target = list(reversed(target))
-        
+
         min_length = min(len(rev_word), len(rev_target))
-        
+        rhyme_score = 0
+
         for i in range(min_length):
             if rev_word[i] == rev_target[i]:
-                wordDict[word] += 1
+                rhyme_score += 1
             else:
                 break
 
-        max_key = max(wordDict, key=wordDict.get)
-        max_value = wordDict[max_key]
+        if rhyme_score > max_value:
+            max_value = rhyme_score
+            max_word = word
 
-    return f"{max_key} {max_value}"
+    return f"{max_word} {max_value}"
 
-words = ["thunder", "blender", "under", "puzzle", "hunder"]
+words = ["thunder", "blender", "under", "puzzle", "hnder"]
 target = "thunder"
 print(rhymingWords(words, target))
